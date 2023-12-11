134fea532e80766852afdd00fd0837d0771ddf98
notzree <notzree@gmail.com>
2023-12-10 22:24:13 -0500

    Update main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use notify::{Error, FsEventWatcher};
 use notify::{RecursiveMode, Watcher};
 use notify_debouncer_full::{new_debouncer, DebouncedEvent, Debouncer, FileIdMap};
 use plist::Value;
+use setup::read_config;
 use std::fs::metadata;
 use std::io;
 use std::{
@@ -18,6 +19,20 @@ pub mod cli;
 pub mod setup;
 use dotenv::dotenv;
 use tokio::*;
+
+#[derive(Debug, Clone)]
+pub struct File {
+    name: String,
+    url: String,
+    path: String,
+}
+
+enum ParseEventResult {
+    File(File),
+    Empty,
+}
+
+// TODO: Make an Enum out of the courses that have been added to the config file
 
 #[tokio::main]
 async fn main() {
@@ -45,6 +60,9 @@ fn start_monitor() -> std::result::Result<(), Box<dyn std::error::Error>> {
     log::info!("Starting monitor...");
     let gpt_client =
         ChatGPT::new(std::env::var("GPT_API_KEY").expect("OpenAI API key required")).unwrap();
+    let mut conversation: Conversation = gpt_client.new_conversation_directed(
+        "You are a LLM designed to categorize downloaded files into their  ",
+    );
     let (action_tx, mut action_rx) = tokio::sync::mpsc::unbounded_channel();
     let config = setup::read_config()?;
     let download_path = config.download_path;
@@ -56,8 +74,16 @@ fn start_monitor() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     tokio::spawn(async move {
         while let Some(event) = action_rx.recv().await {
-            // Do something with the event
-            handle_event(event, gpt_client.clone());
+            let file = parse_event(event);
+            match file {
+                Ok(ParseEventResult::File(file)) => {
+                    
+                }
+                Ok(_) => {}
+                Err(e) => {
+                    log::error!("Error: {}", e);
+                }
+            }
         }
     });
 
@@ -88,42 +114,67 @@ fn create_debouncer<P: AsRef<Path>>(
     Ok((debouncer, ()))
 }
 
-fn handle_event(
+fn parse_event(
     event: DebouncedEvent,
-    client: ChatGPT,
-) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    match event.event.kind {
-        EventKind::Create(kind) => {
-            if kind == CreateKind::File {
-                // log::info!("Downloaded file");
-                let path = event.event.paths[0].to_str().unwrap();
-                let metadata = std::fs::metadata(path)?;
-                match get_where_froms_attribute(path)? {
-                    Some(data) => {
-                        log::info!("Attribute found");
-                        let reader = io::Cursor::new(data);
-                        match plist::Value::from_reader(reader) {
-                            Ok(value) => {
-                                if let Value::Array(array) = value {
-                                    for item in array {
-                                        if let Value::String(url) = item {
-                                            log::info!("URL: {}", url);
-                                        }
-                                    }
-                                }
-                            }
-                            Err(e) => eprintln!("Failed to parse plist: {}", e),
-                        }
-                    }
-                    None => println!("Attribute not found"),
+) -> std::result::Result<ParseEventResult, Box<dyn std::error::Error>> {
+    if let EventKind::Create(CreateKind::File) = event.event.kind {
+        let path = event
+            .event
+            .paths
+            .get(0)
+            .and_then(|p| p.to_str())
+            .ok_or("Invalid path")?;
+
+        let data = get_where_froms_attribute(path)?.ok_or("Attribute not found")?;
+
+        let reader = io::Cursor::new(data);
+        let plist_value = plist::Value::from_reader(reader)
+            .map_err(|e| format!("Failed to parse plist: {}", e))?;
+
+        if let Value::Array(array) = plist_value {
+            for item in array {
+                if let Value::String(url) = item {
+                    let file_name = extract_filename(path).ok_or("Failed to extract filename")?;
+
+                    return Ok(ParseEventResult::File(File {
+                        name: file_name.to_string(),
+                        url,
+                        path: path.to_string(),
+                    }));
                 }
             }
         }
-        _ => {}
     }
-    Ok(())
+
+    Ok(ParseEventResult::Empty)
 }
 
-fn get_where_froms_attribute(file_path: &str) -> io::Result<Option<Vec<u8>>> {
+fn craft_staring_prompt() -> String {
+    let config = read_config().unwrap();
+    let courses = config.courses;
+
+    let prompt = format!("
+    Your job is to determine whether a downloaded folder is a file that corresponds to one of the following courses: {:#?}
+    \n
+    If so, return the course name. If not, return the string NONE.\n
+    You will be given the URL of where the file was downloaded from and the name of the file.
+    If the URL contains 'learn.uwaterloo.ca', it is highly likely but not guaranteed that the file is a course file.
+    You should check to see if the file name or the URL contains the course code or the course name.", courses);
+    return prompt.to_string();
+}
+
+fn get_file_contents(file_path: &str) -> std::result::Result<Vec<u8>, std::io::Error> {
+    std::fs::read(file_path)
+}
+
+fn grep_course_code(file: File)->
+
+fn get_where_froms_attribute(
+    file_path: &str,
+) -> std::result::Result<std::option::Option<Vec<u8>>, std::io::Error> {
     xattr::get(file_path, "com.apple.metadata:kMDItemWhereFroms")
+}
+
+fn extract_filename(file_path: &str) -> Option<&str> {
+    file_path.rsplit('/').next()
 }
